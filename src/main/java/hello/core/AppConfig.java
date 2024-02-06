df43ffac6a8070cab759a14d92aa6333347e12f8
gimtaehyeong <adsds126@gmail.com>
2024-02-06 11:31:07 +0900

    singleton test

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,23 +8,32 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 //공연기획자 역할을 해주는 클래스
+@Configuration
 public class AppConfig {
     //생성자 주입
+    @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
-
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(
                 memberRepository(),
                 discountPolicy()
         );
     }
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new FixDiscountPolicy();
     }
